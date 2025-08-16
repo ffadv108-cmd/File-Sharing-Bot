298e5d7d4e619b61505f0a30ed379d01860f4029
ffadv108-cmd <ffadv108@gmail.com>
2025-08-16 20:09:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,22 +5,21 @@ from logging.handlers import RotatingFileHandler
 
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", "21283994"))
-API_HASH = os.environ.get("API_HASH", "48a6742d2bc2f1b914f6243ccd1cccab")
+API_ID = int(os.environ.get("API_ID", "29000942"))
+API_HASH = os.environ.get("API_HASH", "145b64f961bebca42d960722c748d59e")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "7077129266"))
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://igsweety293:N8fluQM26K5biQb1@cluster0.e7zis.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = int(os.environ.get("OWNER_ID", "6934060520"))
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://ayannhi05:qEUzhnntyWOaY2er@cluster0.rzquzxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "igsweety293")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002548081189"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002194487459"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002607004114"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002501079870"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002544320786"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002667648025"))
 
 
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "300")) # auto delete in seconds
 
 
 PORT = os.environ.get("PORT", "8080")
@@ -30,7 +29,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 try:
     ADMINS=[6848088376]
-    for x in (os.environ.get("ADMINS", "7077129266").split()):
+    for x in (os.environ.get("ADMINS", "6934060520").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -67,7 +66,7 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need 
 
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(7077129266)
+ADMINS.append(6934060520)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
